18cec950f1b70fd79c1f58c3e24d0b37dd33e772
vanessa.cardoso <vanessacardoso.santos@gmail.com>
2021-04-19 11:20:44 -0300

    Projeto criado utilizando rest template entre os microservices

---
+++ hr-payroll/src/main/java/com/hrpayroll/hrpayroll/service/PaymentService.java
@@ -0,0 +1,28 @@
+package com.hrpayroll.hrpayroll.service;
+
+import com.hrpayroll.hrpayroll.entity.Payment;
+import com.hrpayroll.hrpayroll.entity.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class PaymentService {
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Payment getPayment(long workerId, int days) {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", "" + workerId);
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
+    }
+}
